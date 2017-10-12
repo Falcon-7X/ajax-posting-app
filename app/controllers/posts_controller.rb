de456964d04639f3d8db4b0bc5c4f945399ec251
fengkai <328458211@qq.com>
2017-10-12 11:16:43 +0800

    average score

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,10 +58,24 @@ class PostsController < ApplicationController
   end
 
   def update
+    sleep(1)
     @post = Post.find(params[:id])
     @post.update!( post_params )
 
     render :json => { :id => @post.id, :message => "ok"}
+  end
+
+  def rate
+    @post = Post.find(params[:id])
+
+    existing_score = @post.find_score(current_user)
+    if existing_score
+      existing_score.update( :score => params[:score] )
+    else
+      @post.scores.create( :score => params[:score], :user => current_user )
+    end
+
+    render :json => { :average_score => @post.average_score }
   end
 
   protected
